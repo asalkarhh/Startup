7aafa8df5e5fb7d3754677c1114120b085e2173e
ItsMeSMT7 <asalkarsumit@gmail.com>
2026-04-10 12:31:25 +0530

    Update speed performance

--- src/index.js
+++ src/index.js
@@ -5,19 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'aos/dist/aos.css';
 import './styles/main.css';
 import App from './App';
-import searchLogo from './assets/searchlogo.png';
-
-// Dynamically update the favicon from the assets folder
-const existingLink = document.querySelector("link[rel~='icon']");
-if (existingLink) {
-  document.head.removeChild(existingLink);
-}
-
-const newLink = document.createElement('link');
-newLink.rel = 'icon';
-newLink.type = 'image/png';
-newLink.href = searchLogo;
-document.head.appendChild(newLink);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
